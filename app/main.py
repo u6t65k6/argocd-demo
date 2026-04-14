8764a98c6fca32794823ba614334e441eb540cd5
u6t65k6 <u.6t65k6@gmail.com>
2026-04-14 15:03:10 +0800

    Update application version to v5.4

--- app/main.py
+++ app/main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {
-        "version": os.getenv("APP_VERSION", "v5.3"),
+        "version": os.getenv("APP_VERSION", "v5.4"),
         "status": "running very smoothly",
     }
