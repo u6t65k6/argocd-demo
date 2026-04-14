5f94f0c579811bba19bb3443314ac34a76048b21
yu <u.6t65k6@gmail.com>
2026-04-14 16:41:58 +0800

    Revert "Update app version to v6.1"
    This reverts commit 5c94405ed268044de16077f2d401808ab542c5ae.

--- app/main.py
+++ app/main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {
-        "version": os.getenv("APP_VERSION", "v6.1"),
+        "version": os.getenv("APP_VERSION", "v6.0"),
         "status": "running finally",
        }
